addf5e6c8b1cd231f866f4190df1110102bbe5c3
iloveusername <bchasestair@gmail.com>
2023-01-10 13:13:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,27 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
 env = gym.make('CartPole-v1')
 
-is_ipython = 'inline' in matplotlib.get_backend()
-print(is_ipython)
+plt.ion()
+
+device = torch.device("cpu")
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
